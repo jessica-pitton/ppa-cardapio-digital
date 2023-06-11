b5e4c3ff5d433da4a98671c519239aa53f8def64
jessica-pitton <je.pitton@gmail.com>
2023-06-11 17:54:02 -0300

    Inclusão de permissão para acesso a paginas

--- Cardapio/cardapioweb/produto/views.py
+++ Cardapio/cardapioweb/produto/views.py
@@ -1,7 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.urls import reverse
 from .models import Produto
 
+@login_required
 def listar_produtos(request):
 
     produtos = Produto.objects.all()
